0fa86689d90635c924cf5189acd265cdcf66681f
Thasneem Abdul Saleem Mohamed Thasneem <abdulsal@student.curtin.edu.au>
2022-10-08 09:34:05 +0530

    Model Class are added

---
+++ WebGui/Models/Booking.cs
@@ -0,0 +1,9 @@
+﻿namespace WebGui.Models
+{
+    public class Booking
+    {
+        public string PersonName { get; set; }
+        public Nullable<System.DateTime> StartDate { get; set; }
+        public Nullable<System.DateTime> EndDate { get; set; }
+    }
+}
---
+++ WebGui/Models/Centre.cs
@@ -0,0 +1,11 @@
+﻿namespace WebGui.Models
+{
+    public class Centre
+    {
+        public string CentreName { get; set; }
+        public Nullable<System.DateTime> StartDate { get; set; }
+        public Nullable<System.DateTime> EndDate { get; set; }
+        public Nullable<int> BookStatus { get; set; }
+        public string BookedUserName { get; set; }
+    }
+}
